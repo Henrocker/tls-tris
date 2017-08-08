32cda107f5d9b840d607ad093df57538f1fab8f4
Watson Ladd <watson@cloudflare.com>
2017-08-08 13:32:10 -0700

    Fix resumption bug

--- handshake_server.go
+++ handshake_server.go
@@ -375,8 +375,8 @@ func (hs *serverHandshakeState) checkForResumption() bool {
 		return false
 	}
 
-	// Upgrade if client now supports EMS
-	if hs.clientHello.extendedMSSupported && !hs.sessionState.usedEMS {
+	// Do not resume connections where client support for EMS has changed
+	if hs.clientHello.extendedMSSupported != hs.sessionState.usedEMS {
 		return false
 	}
 
